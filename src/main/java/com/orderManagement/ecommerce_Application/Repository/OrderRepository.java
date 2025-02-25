69d84fb688af61a81c90686c6c100e07550c8210
Sudhakar.Mandaleeka <mandalikasudhakar@gmail.com>
2025-02-24 19:44:24 -0500

    added packages

--- src/main/java/com/orderManagement/ecommerce_Application/Repository/OrderRepository.java
+++ src/main/java/com/orderManagement/ecommerce_Application/Repository/OrderRepository.java
@@ -1,5 +1,6 @@
-package com.orderManagement.ecommerce_Application;
+package com.orderManagement.ecommerce_Application.Repository;
 
+import com.orderManagement.ecommerce_Application.Model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
